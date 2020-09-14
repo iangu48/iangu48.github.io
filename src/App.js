a764a3aba04a185bbeea20774e09297ac550df18
Ian <iangu234@gmail.com>
2020-09-14 12:35:14 -0400

    update contact me section and apple touch icon

--- src/App.js
+++ src/App.js
@@ -573,7 +573,18 @@ function ContactSection() {
         <Row className={"h100"} justify={"center"} align={"middle"}>
             <Col offset={1}>
                 <Title level={2} style={{color: "white", marginTop: "30px", fontWeight: 300}}>Contact Me</Title>
+                    <Space direction={"horizontal"} align={"baseline"} size={15}>
+                        <Button icon={<LinkedinFilled />} type={"primary"} href={"https://www.linkedin.com/in/iancgu/"} shape={"circle"}>
+                        </Button>
+                        <Button icon={<GithubFilled />} type={"primary"} href={"https://github.com/iangu48"} shape={"circle"}>
+                        </Button>
+                        <Paragraph copyable code>
+                            <a href={"mailto:ian.gu@mail.utoronto.ca"} style={{color: "white"}}>
+                                ian.gu@mail.utoronto.ca
+                            </a>
+                        </Paragraph>
 
+                    </Space>
 
             </Col>
         </Row>
